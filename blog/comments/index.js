9d2c504e4618fc7d1ec2e6a878cfb8725559f0db
skytech001 <abiodunlabinjo@gmail.com>
2023-04-04 21:32:45 -0500

    Adding initial files

---
+++ blog/comments/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+  const postId = req.params.id;
+
+  const comments = commentsByPostId[postId] || [];
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[postId] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("comments is listening on port 4001");
+});
